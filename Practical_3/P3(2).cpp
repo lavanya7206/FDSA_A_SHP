9bdee1e7df50450cf3434b6bdc47dd9a5268e369
Lavanya <parekhlavanya@gmail.com>
2026-08-17 22:25:02 +0530

    Update P3(2).cpp

--- Practical_3/P3(2).cpp
+++ Practical_3/P3(2).cpp
@@ -3,37 +3,33 @@ using namespace std;
 int main()
 {
     int n;
-    cout << "Enter number of buckets: ";
+    cout << "Enter number of elements: ";
     cin >> n;
-    int a[n];
-    cout << "Enter colour codes (0, 1, 2):\n";
-    for(int i = 0; i < n; i++)
-        cin >> a[i];
-    int low = 0, mid = 0, high = n - 1;
-    while(mid <= high)
+    int arr[n];
+    cout << "Enter colour codes (0, 1, 2): ";
+    for (int i = 0; i < n; i++)
     {
-        if(a[mid] == 0)
+        cin >> arr[i];
+    }
+    int count[3] = {0, 0, 0};
+    for (int i = 0; i < n; i++)
+    {
+        count[arr[i]]++;
+    }
+    int index = 0;
+    for (int colour = 0; colour <= 2; colour++)
+    {
+        while (count[colour] > 0)
         {
-            int temp = a[low];
-            a[low] = a[mid];
-            a[mid] = temp;
-            low++;
-            mid++;
-        }
-        else if(a[mid] == 1)
-        {
-            mid++;
-        }
-        else
-        {
-            int temp = a[mid];
-            a[mid] = a[high];
-            a[high] = temp;
-            high--;
+            arr[index] = colour;
+            index++;
+            count[colour]--;
         }
     }
     cout << "Sorted colour codes: ";
-    for(int i = 0; i < n; i++)
-        cout << a[i] << " ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
     return 0;
 }
