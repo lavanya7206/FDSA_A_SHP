da33d3a66d86e2b672204ae9d4812950aee19709
Lavanya <parekhlavanya@gmail.com>
2026-08-25 13:34:47 +0530

    Update P4(2).cpp

--- Practical_4/P4(2).cpp
+++ Practical_4/P4(2).cpp
@@ -1,77 +1,78 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-struct Node {
-    int token;
-    Node* next;
+struct Node
+{
+    int data;
+    Node *next;
+    Node *prev;
 };
-void insertEnd(Node*& head, int value) {
-    Node* newNode = new Node;
-    newNode->token = value;
-    newNode->next = NULL;
-    if (head == NULL) {
-        head = newNode;
+Node *front=NULL;
+Node *rear=NULL;
+void insert(int x)
+{
+    Node *newNode=new Node;
+    newNode->data=x;
+    newNode->next=NULL;
+    newNode->prev=rear;
+    if (front==NULL)
+        front=rear=newNode;
+    else
+    {
+        rear->next=newNode;
+        rear=newNode;
+    }
+}
+void deleteValue(int x)
+{
+    Node *temp=front;
+    while (temp!=NULL && temp->data!=x)
+        temp=temp->next;
+    if (temp==NULL)
+    {
+        cout<<"Not found"<<endl;
         return;
     }
-    Node* temp = head;
-    while (temp->next != NULL) {
-        temp = temp->next;
-    }
-    temp->next = newNode;
+    if (temp==front)
+        front=temp->next;
+    else
+        temp->prev->next=temp->next;
+    if (temp==rear)
+        rear=temp->prev;
+    else
+        temp->next->prev=temp->prev;
+    delete temp;
 }
-void deleteValue(Node*& head, int value) {
-    if (head == NULL) {
-        cout << "Queue is empty!" << endl;
-        return;
+void forward()
+{
+    Node *temp=front;
+    cout<<"Front to Rear:";
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
     }
-    if (head->token == value) {
-        Node* temp = head;
-        head = head->next;
-        delete temp;
-        return;
-    }
-    Node* temp = head;
-    while (temp->next != NULL) {
-        if (temp->next->token == value) {
-            Node* deleteNode = temp->next;
-            temp->next = deleteNode->next;
-            delete deleteNode;
-            return;
-        }
-        temp = temp->next;
-    }
-    cout << "Token not found!" << endl;
+    cout<<endl;
 }
-void display(Node* head) {
-    Node* temp = head;
-    cout << "Front to Back: ";
-    while (temp != NULL) {
-        cout << temp->token << " ";
-        temp = temp->next;
+void reverse()
+{
+    Node*temp=rear;
+    cout<<"Rear to Front:";
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->prev;
     }
-    cout << endl;
+    cout<<endl;
 }
-void reversePrint(Node* head) {
-    if (head == NULL) {
-        return;
-    }
-    reversePrint(head->next);
-    cout << head->token << " ";
-}
-int main() {
-    Node* head = NULL;
-    insertEnd(head, 101);
-    insertEnd(head, 102);
-    insertEnd(head, 103);
-    insertEnd(head, 104);
-    cout << "Original Queue:" << endl;
-    display(head);
-    cout << "\nAfter deleting patient 102:" << endl;
-    deleteValue(head, 102);
-    display(head);
-    cout << "\nForward Traversal:" << endl;
-    display(head);
-    cout << "\nReverse Printing (Back to Front): ";
-    reversePrint(head);
-    cout << endl;
+int main()
+{
+    insert(10);
+    insert(20);
+    insert(30);
+    insert(40);
+    forward();
+    deleteValue(20);
+    forward();
+    reverse();
     return 0;
 }
